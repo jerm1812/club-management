e39a38d93bcabbb7925cac7561fd4179a0985668
Jerm1812 <jeremy@cookbunch.net>
2022-04-30 12:51:09 -0600

    fix

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../'));
+  res.sendFile(path.join(__dirname, '../public/pages/index.html'));
 });
 
 router.get('/stylesheet', function(req, res, next) {
