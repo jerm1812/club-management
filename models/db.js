3ce6630cb555a4d2e7f5b7d1427ce3c3366376d9
emmarslayton <emma.r.slayton@gmail.com>
2022-04-30 13:55:54 -0600

    adding database

--- models/db.js
+++ models/db.js
@@ -5,7 +5,27 @@ sql.setDialect('mssql');
 var user = sql.define({
     name: 'user',
     columns: [
-        'id', 
-        'name'
+        'user_id', 
+        'username',
+        'user_pw',
+        'email',
+        'role_id',
+        'group_id'
+    ]
+})
+
+var user = sql.define({
+    name: 'role',
+    columns: [
+        'role_id',
+        'role_name'
+    ]
+})
+
+var user = sql.define({
+    name: 'group', 
+    columns: [
+        'group_id',
+        'group_name'
     ]
 })
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const req = require('express/lib/request');
 var router = express.Router();
 
 /* GET home page. */
